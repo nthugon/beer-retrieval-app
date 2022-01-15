5c7cd2dc4133bec8fa550272d46ba4c348340d38
Nathan Hugon <n.hugon@f5.com>
2022-01-14 16:52:31 -0800

    refactor: Upgrades to Angular 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { HomeComponent } from './components/home/home.component';
-import { BeersComponent } from './components/beers/beers.component';
-import { NotFoundComponent } from './shared/not-found/not-found.component';
-import { BeerComponent } from './components/beer/beer.component';
-import { LoaderComponent } from './shared/loader/loader.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./shared/header/header.component";
+import { FooterComponent } from "./shared/footer/footer.component";
+import { HomeComponent } from "./components/home/home.component";
+import { BeersComponent } from "./components/beers/beers.component";
+import { NotFoundComponent } from "./shared/not-found/not-found.component";
+import { BeerComponent } from "./components/beer/beer.component";
+import { LoaderComponent } from "./shared/loader/loader.component";
 
 @NgModule({
   declarations: [
@@ -21,15 +20,10 @@ import { LoaderComponent } from './shared/loader/loader.component';
     BeersComponent,
     NotFoundComponent,
     BeerComponent,
-    LoaderComponent
+    LoaderComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    AngularFontAwesomeModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
